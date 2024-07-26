6439c081a7b181572449f3734282756c7fa29ba2
FireHR2004 <fatir12iksan@gmail.com>
2024-07-26 10:28:41 +0700

    Update Halaman Profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -5,9 +5,15 @@
         <h1 style="color: black;">Profile</h1>
         <br>
         <div class="card">
-            <div class="card-body">
-                <h4>{{ Auth::user()->name }}</h4>
-                <h5>{{ Auth::user()->email }}</h5>
+            <div class="card-body d-flex">
+                <div class="mx-3">
+                    <img src="{{ asset('image/userProfile.jpg') }}" class="rounded-circle" alt="Profile Picture" width="250px"
+                        height="250px">
+                </div>
+                <div class="mx-3 w-full">
+                    <h1>{{ Auth::user()->name }}</h1>
+                    <h5>{{ Auth::user()->email }}</h5>
+                </div>
             </div>
         </div>
     </div>
