5c81a1fc00eb558c8a3a018c76ee5b0bc4a572a3
veritas39 <m.bryan.alcik@gmail.com>
2024-07-26 10:25:40 +0700

    update until NSP

--- app/Http/Controllers/PerhitunganController.php
+++ app/Http/Controllers/PerhitunganController.php
@@ -5,185 +5,69 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Scoring;
 use App\Models\Kriteria;
+use App\Models\Alternatif;
 
 class PerhitunganController extends Controller
 {
     public function index()
     {
+        // Fetch scoring data with related models
         $scorings = Scoring::with(['alternatif', 'kriteria', 'subKriteria'])->get();
         $kriteria = Kriteria::all();
+        $alternatifs = Alternatif::all();
 
-        // Calculate averages for each kriteria
-        $averages = [];
+        // Calculate the average for each criteria
+        $kriteriaAverage = [];
         foreach ($kriteria as $k) {
-            $total = $scorings->where('kriteria_id', $k->id)
-                ->sum(function ($scoring) {
-                    return $scoring->subKriteria->nilai_sub_kriteria * $scoring->kriteria->bobot_kriteria;
-                });
-            $count = $scorings->where('kriteria_id', $k->id)->count();
-            $averages[$k->id] = $count ? $total / $count : 0;
+            $total = $scorings->where('kriteria_id', $k->id)->sum(function ($scoring) {
+                return $scoring->subKriteria->nilai_sub_kriteria;
+            });
+            $average = $total / $alternatifs->count();
+            $kriteriaAverage[$k->id] = $average;
         }
 
-        // Calculate PDA, NDA, SP, and SN
-        $pdaNda = [];
-        $spSn = [];
-        foreach ($scorings->groupBy('alternatif_id') as $alternatif_id => $scoring_group) {
-            $pda = [];
-            $nda = [];
-            $sp = 0;
-            $sn = 0;
-
-            foreach ($kriteria as $k) {
+        // Calculate SP values and total SP
+        $maxTotalSP = 0;
+        $spTotals = [];
+        foreach($scorings->groupBy('alternatif_id') as $alternatif_id => $scoring_group) {
+            $spValues = [];
+            foreach($kriteria as $k) {
                 $scoring = $scoring_group->where('kriteria_id', $k->id)->first();
-                $nilai = $scoring ? $scoring->subKriteria->nilai_sub_kriteria : 0;
-                $bobot = $k->bobot_kriteria;
-                $average = $averages[$k->id] ?? 0;
-
-                $nilai_bobot = $nilai * $bobot;
-
-                if ($average > 0) {
-                    if ($k->jenis_kriteria == 'Benefit') {
-                        $pda[$k->id] = max(0, ($nilai_bobot - $average)) / $average;
-                        $nda[$k->id] = max(0, ($average - $nilai_bobot)) / $average;
-                    } else { // Cost
-                        $pda[$k->id] = max(0, ($average - $nilai_bobot)) / $average;
-                        $nda[$k->id] = max(0, ($nilai_bobot - $average)) / $average;
-                    }
-                } else {
-                    $pda[$k->id] = 0;
-                    $nda[$k->id] = 0;
-                }
-
-                // Sum up SP and SN
-                $sp += $pda[$k->id];
-                $sn += $nda[$k->id];
+                $nilaiSubKriteria = $scoring ? $scoring->subKriteria->nilai_sub_kriteria : 0;
+                $average = $kriteriaAverage[$k->id];
+                $pdaValue = ($nilaiSubKriteria - $average) / $average;
+                $pdaValue = $pdaValue < 0 ? 0 : $pdaValue;
+                $spValue = $pdaValue * $k->bobot_kriteria;
+                $spValues[] = $spValue;
             }
-
-            $pdaNda[$alternatif_id] = ['pda' => $pda, 'nda' => $nda];
-            $spSn[$alternatif_id] = ['sp' => $sp, 'sn' => $sn];
+            $totalSP = array_sum($spValues);
+            $spTotals[$alternatif_id] = $totalSP;
+            if ($totalSP > $maxTotalSP) {
+                $maxTotalSP = $totalSP;
+            }
         }
 
-        // Calculate NSP and NSN for each alternative
-        $maxSN = max(array_column($spSn, 'sn'));
-
-        $nsp = [];
-        $nsn = [];
-        $as = []; // Array to hold Average Score (AS) values
-
-        foreach ($spSn as $alternatif_id => $values) {
-            $sp = $values['sp'];
-            $sn = $values['sn'];
-
-            $totalWeight = array_sum(array_column($kriteria->toArray(), 'bobot_kriteria'));
-
-            $nsp[$alternatif_id] = $totalWeight ? $sp / $totalWeight : 0;
-            $nsn[$alternatif_id] = $maxSN > 0 ? 1 - ($sn / $maxSN) : 0;
-
-            // Calculate AS and ensure it is between 0 and 1
-            $as[$alternatif_id] = max(0, min(1, 0.5 * ($nsp[$alternatif_id] + $nsn[$alternatif_id])));
-        }
-
-        return view('perhitungan.index', [
-            'scorings' => $scorings,
-            'kriteria' => $kriteria,
-            'averages' => $averages,
-            'pdaNda' => $pdaNda,
-            'spSn' => $spSn,
-            'nsp' => $nsp,
-            'nsn' => $nsn,
-            'as' => $as
-        ]);
-    
-    }
-
-    public function ranking()
-    {
-        $scorings = Scoring::with(['alternatif', 'kriteria', 'subKriteria'])->get();
-        $kriteria = Kriteria::all();
-
-        // Calculate averages for each kriteria
-        $averages = [];
-        foreach ($kriteria as $k) {
-            $total = $scorings->where('kriteria_id', $k->id)
-                ->sum(function ($scoring) {
-                    return $scoring->subKriteria->nilai_sub_kriteria * $scoring->kriteria->bobot_kriteria;
-                });
-            $count = $scorings->where('kriteria_id', $k->id)->count();
-            $averages[$k->id] = $count ? $total / $count : 0;
-        }
-
-        // Calculate PDA, NDA, SP, and SN
-        $pdaNda = [];
-        $spSn = [];
-        foreach ($scorings->groupBy('alternatif_id') as $alternatif_id => $scoring_group) {
-            $pda = [];
-            $nda = [];
-            $sp = 0;
-            $sn = 0;
-
-            foreach ($kriteria as $k) {
+        // Calculate NP and NSN values
+        $maxTotalNP = 0;
+        $npTotals = [];
+        foreach($scorings->groupBy('alternatif_id') as $alternatif_id => $scoring_group) {
+            $npValues = [];
+            foreach($kriteria as $k) {
                 $scoring = $scoring_group->where('kriteria_id', $k->id)->first();
-                $nilai = $scoring ? $scoring->subKriteria->nilai_sub_kriteria : 0;
-                $bobot = $k->bobot_kriteria;
-                $average = $averages[$k->id] ?? 0;
-
-                $nilai_bobot = $nilai * $bobot;
-
-                if ($average > 0) {
-                    if ($k->jenis_kriteria == 'Benefit') {
-                        $pda[$k->id] = max(0, ($nilai_bobot - $average)) / $average;
-                        $nda[$k->id] = max(0, ($average - $nilai_bobot)) / $average;
-                    } else { // Cost
-                        $pda[$k->id] = max(0, ($average - $nilai_bobot)) / $average;
-                        $nda[$k->id] = max(0, ($nilai_bobot - $average)) / $average;
-                    }
-                } else {
-                    $pda[$k->id] = 0;
-                    $nda[$k->id] = 0;
-                }
-
-                // Sum up SP and SN
-                $sp += $pda[$k->id];
-                $sn += $nda[$k->id];
+                $nilaiSubKriteria = $scoring ? $scoring->subKriteria->nilai_sub_kriteria : 0;
+                $average = $kriteriaAverage[$k->id];
+                $npValue = ($nilaiSubKriteria - $average) / $average;
+                $npValue = $npValue < 0 ? 0 : $npValue;
+                $npValue *= $k->bobot_kriteria;
+                $npValues[] = $npValue;
             }
-
-            $pdaNda[$alternatif_id] = ['pda' => $pda, 'nda' => $nda];
-            $spSn[$alternatif_id] = ['sp' => $sp, 'sn' => $sn];
+            $totalNP = array_sum($npValues);
+            $npTotals[$alternatif_id] = $totalNP;
+            if ($totalNP > $maxTotalNP) {
+                $maxTotalNP = $totalNP;
+            }
         }
 
-        // Calculate NSP and NSN for each alternative
-        $maxSN = max(array_column($spSn, 'sn'));
-
-        $nsp = [];
-        $nsn = [];
-        $as = []; // Array to hold Average Score (AS) values
-
-        foreach ($spSn as $alternatif_id => $values) {
-            $sp = $values['sp'];
-            $sn = $values['sn'];
-
-            $totalWeight = array_sum(array_column($kriteria->toArray(), 'bobot_kriteria'));
-
-            $nsp[$alternatif_id] = $totalWeight ? $sp / $totalWeight : 0;
-            $nsn[$alternatif_id] = $maxSN > 0 ? 1 - ($sn / $maxSN) : 0;
-
-            // Calculate AS and ensure it is between 0 and 1
-            $as[$alternatif_id] = max(0, min(1, 0.5 * ($nsp[$alternatif_id] + $nsn[$alternatif_id])));
-        }
-
-        // Sort alternatives by AS and rank them
-        $rankedAlternatives = collect($as)->sortDesc()->values()->all();
-        $rankings = [];
-        foreach ($rankedAlternatives as $key => $value) {
-            $rankings[array_search($value, $as)] = $key + 1; // Rank starts at 1
-        }
-
-        return view('result.index', [
-            'scorings' => $scorings,
-            'kriteria' => $kriteria,
-            'as' => $as,
-            'rankings' => $rankings
-        ]);
+        return view('perhitungan.index', compact('scorings', 'kriteria', 'alternatifs', 'kriteriaAverage', 'spTotals', 'maxTotalSP', 'npTotals', 'maxTotalNP'));
     }
-
 }
--- resources/views/perhitungan/index.blade.php
+++ resources/views/perhitungan/index.blade.php
@@ -3,16 +3,17 @@
 @section('content')
     <div class="container">
         <h1>Perhitungan</h1>
-        <br>
 
+        <!-- Title for the current table -->
         <h2>Data Awal</h2>
-        <table class="table table-bordered" style="width: 100%;">
+        <!-- Display the current table -->
+        <table class="table table-bordered">
             <thead>
                 <tr>
-                    <th style="width: 100px;">No</th>
-                    <th style="width: 200px;">Alternatif</th>
+                    <th>No</th>
+                    <th>Alternatif</th>
                     @foreach($kriteria as $k)
-                        <th style="width: 150px;">Score {{ $k->nama_kriteria }}</th>
+                        <th>Score {{ $k->nama_kriteria }}</th>
                     @endforeach
                 </tr>
             </thead>
@@ -34,44 +35,17 @@
             </tbody>
         </table>
 
-        <h2>Data Score</h2>
-        <table class="table table-bordered" style="width: 100%;">
+        <!-- Title for the kriteria table -->
+        <h2>Daftar Kriteria</h2>
+        <!-- Display the kriteria table -->
+        <table class="table table-bordered">
             <thead>
                 <tr>
-                    <th style="width: 100px;">No</th>
-                    <th style="width: 200px;">Alternatif</th>
-                    @foreach($kriteria as $k)
-                        <th style="width: 150px;">Score {{ $k->nama_kriteria }}</th>
-                    @endforeach
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($scorings->groupBy('alternatif_id') as $alternatif_id => $scoring_group)
-                    <tr>
-                        <td>{{ $alternatif_id }}</td>
-                        <td>{{ $scoring_group->first()->alternatif->nama_alternatif }}</td>
-                        @foreach($kriteria as $k)
-                            <td>
-                                @php
-                                    $scoring = $scoring_group->where('kriteria_id', $k->id)->first();
-                                    echo $scoring && $scoring->subKriteria ? $scoring->subKriteria->nilai_sub_kriteria : 'N/A';
-                                @endphp
-                            </td>
-                        @endforeach
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-
-        <h2>Daftar Bobot</h2>
-        <table class="table table-bordered" style="width: 100%;">
-            <thead>
-                <tr>
-                    <th style="width: 100px;">ID</th>
-                    <th style="width: 150px;">Kode Kriteria</th>
-                    <th style="width: 250px;">Nama Kriteria</th>
-                    <th style="width: 150px;">Bobot Kriteria</th>
-                    <th style="width: 150px;">Jenis Kriteria</th>
+                    <th>ID</th>
+                    <th>Kode Kriteria</th>
+                    <th>Nama Kriteria</th>
+                    <th>Bobot Kriteria</th>
+                    <th>Jenis Kriteria</th>
                 </tr>
             </thead>
             <tbody>
@@ -87,11 +61,12 @@
             </tbody>
         </table>
 
-        <h2>Average Solution</h2>
+        <!-- Title for the AV table -->
+        <h2>AV</h2>
+        <!-- Display the AV table -->
         <table class="table table-bordered">
             <thead>
                 <tr>
-                    <th>No</th>
                     <th>Alternatif</th>
                     @foreach($kriteria as $k)
                         <th>{{ $k->kode_kriteria }}</th>
@@ -101,16 +76,12 @@
             <tbody>
                 @foreach($scorings->groupBy('alternatif_id') as $alternatif_id => $scoring_group)
                     <tr>
-                        <td>{{ $alternatif_id }}</td>
                         <td>{{ $scoring_group->first()->alternatif->nama_alternatif }}</td>
                         @foreach($kriteria as $k)
                             <td>
                                 @php
                                     $scoring = $scoring_group->where('kriteria_id', $k->id)->first();
-                                    $nilai = $scoring ? $scoring->subKriteria->nilai_sub_kriteria : 0;
-                                    $bobot = $k->bobot_kriteria;
-                                    $average = $nilai * $bobot;
-                                    echo number_format($average, 2);
+                                    echo $scoring ? $scoring->subKriteria->nilai_sub_kriteria : 'N/A';
                                 @endphp
                             </td>
                         @endforeach
@@ -119,95 +90,226 @@
             </tbody>
         </table>
 
+        <!-- Title for the AV Sum and Average table -->
+        <h2>AV Sum and Average</h2>
+        <!-- Display the AV Sum and Average table -->
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>Kriteria</th>
+                    @foreach($kriteria as $k)
+                        <th>{{ $k->kode_kriteria }}</th>
+                    @endforeach
+                </tr>
+            </thead>
+            <tbody>
+                @php
+                    $kriteriaSum = [];
+                    $kriteriaAverage = [];
+                    $totalAlternatif = $scorings->groupBy('alternatif_id')->count();
+                @endphp
+                <tr>
+                    <td>Sum</td>
+                    @foreach($kriteria as $k)
+                        @php
+                            $sum = $scorings->where('kriteria_id', $k->id)->sum('subKriteria.nilai_sub_kriteria');
+                            $kriteriaSum[$k->id] = $sum;
+                        @endphp
+                        <td>{{ $sum }}</td>
+                    @endforeach
+                </tr>
+                <tr>
+                    <td>Average</td>
+                    @foreach($kriteria as $k)
+                        @php
+                            $average = $kriteriaSum[$k->id] / $totalAlternatif;
+                            $kriteriaAverage[$k->id] = $average;
+                        @endphp
+                        <td>{{ number_format($average, 2) }}</td>
+                    @endforeach
+                </tr>
+            </tbody>
+        </table>
+
+        <!-- Title for the PDA table -->
         <h2>PDA</h2>
+        <!-- Display the PDA table -->
         <table class="table table-bordered">
             <thead>
                 <tr>
-                    <th>No</th>
                     <th>Alternatif</th>
                     @foreach($kriteria as $k)
-                        <th>PDA {{ $k->kode_kriteria }}</th>
-                        <!-- <th>NDA {{ $k->kode_kriteria }}</th> -->
+                        <th>{{ $k->kode_kriteria }}</th>
                     @endforeach
                 </tr>
             </thead>
             <tbody>
-                @foreach($pdaNda as $alternatif_id => $values)
+                @foreach($scorings->groupBy('alternatif_id') as $alternatif_id => $scoring_group)
                     <tr>
-                        <td>{{ $alternatif_id }}</td>
-                        <td>{{ $scorings->where('alternatif_id', $alternatif_id)->first()->alternatif->nama_alternatif }}</td>
+                        <td>{{ $scoring_group->first()->alternatif->nama_alternatif }}</td>
                         @foreach($kriteria as $k)
-                            <td>{{ number_format($values['pda'][$k->id] ?? 0, 2) }}</td>
-                            <!-- <td>{{ number_format($values['nda'][$k->id] ?? 0, 2) }}</td> -->
+                            <td>
+                                @php
+                                    $scoring = $scoring_group->where('kriteria_id', $k->id)->first();
+                                    $nilaiSubKriteria = $scoring ? $scoring->subKriteria->nilai_sub_kriteria : 0;
+                                    $average = $kriteriaAverage[$k->id];
+                                    $pdaValue = ($nilaiSubKriteria - $average) / $average;
+                                    $pdaValue = $pdaValue < 0 ? 0 : $pdaValue;
+                                @endphp
+                                {{ number_format($pdaValue, 2) }}
+                            </td>
                         @endforeach
                     </tr>
                 @endforeach
             </tbody>
         </table>
 
+        <!-- Title for the NDA table -->
         <h2>NDA</h2>
+        <!-- Display the NDA table -->
         <table class="table table-bordered">
             <thead>
                 <tr>
-                    <th>No</th>
                     <th>Alternatif</th>
-                    <!-- <th>PDA {{ $k->kode_kriteria }}</th> -->
                     @foreach($kriteria as $k)
-                        <th>NDA {{ $k->kode_kriteria }}</th>
+                        <th>{{ $k->kode_kriteria }}</th>
                     @endforeach
                 </tr>
             </thead>
             <tbody>
-                @foreach($pdaNda as $alternatif_id => $values)
+                @foreach($scorings->groupBy('alternatif_id') as $alternatif_id => $scoring_group)
                     <tr>
-                        <td>{{ $alternatif_id }}</td>
-                        <td>{{ $scorings->where('alternatif_id', $alternatif_id)->first()->alternatif->nama_alternatif }}</td>
+                        <td>{{ $scoring_group->first()->alternatif->nama_alternatif }}</td>
                         @foreach($kriteria as $k)
-                            <!-- <td>{{ number_format($values['pda'][$k->id] ?? 0, 2) }}</td> -->
-                            <td>{{ number_format($values['nda'][$k->id] ?? 0, 2) }}</td>
+                            <td>
+                                @php
+                                    $scoring = $scoring_group->where('kriteria_id', $k->id)->first();
+                                    $nilaiSubKriteria = $scoring ? $scoring->subKriteria->nilai_sub_kriteria : 0;
+                                    $average = $kriteriaAverage[$k->id];
+                                    $ndaValue = ($average - $nilaiSubKriteria) / $average;
+                                    $ndaValue = $ndaValue < 0 ? 0 : $ndaValue;
+                                @endphp
+                                {{ number_format($ndaValue, 2) }}
+                            </td>
                         @endforeach
                     </tr>
                 @endforeach
             </tbody>
         </table>
 
-        <h2>NSP and NSN</h2>
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th>Alternatif</th>
-                    <th>NSP</th>
-                    <th>NSN</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($nsp as $alternatif_id => $value)
-                    <tr>
-                        <td>{{ $scorings->where('alternatif_id', $alternatif_id)->first()->alternatif->nama_alternatif }}</td>
-                        <td>{{ number_format($value, 2) }}</td>
-                        <td>{{ number_format($nsn[$alternatif_id] ?? 0, 2) }}</td>
-                    </tr>
+        <!-- Title for the SP table -->
+<h2>SP</h2>
+<!-- Display the SP table -->
+<table class="table table-bordered">
+    <thead>
+        <tr>
+            <th>Alternatif</th>
+            @foreach($kriteria as $index => $k)
+                <th>C{{ $index + 1 }}</th>
+            @endforeach
+            <th>Total</th>
+        </tr>
+    </thead>
+    <tbody>
+        @php
+            $maxTotalSP = 0;
+            $spTotals = [];
+        @endphp
+        @foreach($scorings->groupBy('alternatif_id') as $alternatif_id => $scoring_group)
+            @php
+                $spValues = [];
+                foreach($kriteria as $k) {
+                    $scoring = $scoring_group->where('kriteria_id', $k->id)->first();
+                    $nilaiSubKriteria = $scoring ? $scoring->subKriteria->nilai_sub_kriteria : 0;
+                    $average = $kriteriaAverage[$k->id];
+                    $pdaValue = ($nilaiSubKriteria - $average) / $average;
+                    $pdaValue = $pdaValue < 0 ? 0 : $pdaValue;
+                    $spValue = $pdaValue * $k->bobot_kriteria;
+                    $spValues[] = $spValue;
+                }
+                $totalSP = array_sum($spValues);
+                $spTotals[$alternatif_id] = $totalSP;
+                if ($totalSP > $maxTotalSP) {
+                    $maxTotalSP = $totalSP;
+                }
+            @endphp
+            <tr>
+                <td>{{ $alternatifs->find($alternatif_id)->nama_alternatif }}</td>
+                @foreach($spValues as $spValue)
+                    <td>{{ number_format($spValue, 2) }}</td>
                 @endforeach
-            </tbody>
-        </table>
+                <td>{{ number_format($totalSP, 2) }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+
+<!-- Title for the NSP table -->
+<h2>NSP</h2>
+<!-- Display the NSP table -->
+<table class="table table-bordered">
+    <thead>
+        <tr>
+            <th>Alternatif</th>
+            <th>NSP Value</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($spTotals as $alternatif_id => $totalSP)
+            @php
+                $nspValue = $totalSP / $maxTotalSP;
+            @endphp
+            <tr>
+                <td>{{ $alternatifs->find($alternatif_id)->nama_alternatif }}</td>
+                <td>{{ number_format($nspValue, 2) }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
 
 
-        <h2>AS (Average Score)</h2>
+
+        <!-- Title for the NP table -->
+        <h2>NP</h2>
+        <!-- Display the NP table -->
         <table class="table table-bordered">
             <thead>
                 <tr>
                     <th>Alternatif</th>
-                    <th>AS</th>
+                    @foreach($kriteria as $k)
+                        <th>{{ $k->kode_kriteria }}</th>
+                    @endforeach
+                    <th>Total</th>
                 </tr>
             </thead>
             <tbody>
-                @foreach($as as $alternatif_id => $value)
+                @foreach($scorings->groupBy('alternatif_id') as $alternatif_id => $scoring_group)
                     <tr>
-                        <td>{{ $scorings->where('alternatif_id', $alternatif_id)->first()->alternatif->nama_alternatif }}</td>
-                        <td>{{ number_format($value, 2) }}</td>
+                        <td>{{ $scoring_group->first()->alternatif->nama_alternatif }}</td>
+                        @php
+                            $npValues = [];
+                        @endphp
+                        @foreach($kriteria as $k)
+                            @php
+                                $scoring = $scoring_group->where('kriteria_id', $k->id)->first();
+                                $nilaiSubKriteria = $scoring ? $scoring->subKriteria->nilai_sub_kriteria : 0;
+                                $average = $kriteriaAverage[$k->id];
+                                $ndaValue = ($average - $nilaiSubKriteria) / $average;
+                                $ndaValue = $ndaValue < 0 ? 0 : $ndaValue;
+                                $npValue = $ndaValue * $k->bobot_kriteria;
+                                $npValues[] = $npValue;
+                            @endphp
+                            <td>{{ number_format($npValue, 2) }}</td>
+                        @endforeach
+                        @php
+                            $totalNP = array_sum($npValues);
+                        @endphp
+                        <td><strong>{{ number_format($totalNP, 2) }}</strong></td>
                     </tr>
                 @endforeach
             </tbody>
         </table>
     </div>
+
+    
 @endsection
